f041eb6678c74635d1d442829cb40e2750a31315
sallemiines <ines.sallemi@outlook.com>
2017-10-06 17:47:28 +0100

    Working on scenarios

--- seoExpert_module/test/scenario/BO/check_seo_expert_optimizing_urls_webdriverio.js
+++ seoExpert_module/test/scenario/BO/check_seo_expert_optimizing_urls_webdriverio.js
@@ -13,27 +13,34 @@ describe(' Test n°2 = Check the optimizing urls configuration', function () {
 
     after(common.after);
 
-
-
-
-
-
     describe("Check the optimizing urls tab", function (done) {
-    it('should add rule name ', function (done) {
-        global.fctname = this.test.title
-        this.client
-            .waitForExist(this.selector.BO.ModulePageSeoExpert.optimaze_url_tab, 90000)
-            .click(this.selector.BO.ModulePageSeoExpert.optimaze_url_tab)
-            .waitForExist(this.selector.BO.ModulePageSeoExpert.add_rule_button, 90000)
-            .click(this.selector.BO.ModulePageSeoExpert.add_rule_button)
-            .pause(90000)
-            .waitForExist(this.selector.BO.ModulePageSeoExpert.rule_name_input, 90000)
-            .setValue(this.selector.BO.ModulePageSeoExpert.rule_name_input, global.ruleNameInput)
-            .waitForExist(this.selector.BO.ModulePageSeoExpert.next_button, 90000)
-            .click(this.selector.BO.ModulePageSeoExpert.next_button)
-            .pause(2000)
-            .call(done)
-    })
+        it('should click on optimizing url tab ', function (done) {
+            global.fctname = this.test.title
+            this.client
+                .waitForExist(this.selector.BO.ModulePageSeoExpert.optimaze_url_tab, 90000)
+                .click(this.selector.BO.ModulePageSeoExpert.optimaze_url_tab)
+                .waitForExist(this.selector.BO.ModulePageSeoExpert.add_rule_button, 90000)
+                .click(this.selector.BO.ModulePageSeoExpert.add_rule_button)
+                .call(done);
+        });
+
+        it('should add a rule name ', function (done) {
+            global.fctname = this.test.title
+            this.client
+                .waitForExist(this.selector.BO.ModulePageSeoExpert.rule_name_input, 90000)
+                .setValue(this.selector.BO.ModulePageSeoExpert.rule_name_input, global.ruleNameInput)
+                .call(done);
+        });
+
+        it('should click on next button ', function (done) {
+            global.fctname = this.test.title
+            this.client
+                .waitForExist(this.selector.BO.ModulePageSeoExpert.next_button, 90000)
+                .click(this.selector.BO.ModulePageSeoExpert.next_button)
+                .pause(2000)
+                .call(done);
+        });
+
         it('should select a category', function (done) {
             global.fctname = this.test.title
             this.client
@@ -45,9 +52,10 @@ describe(' Test n°2 = Check the optimizing urls configuration', function () {
                 .waitForExist(this.selector.BO.ModulePageSeoExpert.next_button, 90000)
                 .click(this.selector.BO.ModulePageSeoExpert.next_button)
                 .pause(2000)
-                .call(done)
-        })
-        it('should configure the url', function (done) {
+                .call(done);
+        });
+
+        it('should configure the rule', function (done) {
             global.fctname = this.test.title
             this.client
                 .waitForExist(this.selector.BO.ModulePageSeoExpert.link_rewrite_input, 90000)
@@ -58,38 +66,47 @@ describe(' Test n°2 = Check the optimizing urls configuration', function () {
                 .click(this.selector.BO.ModulePageSeoExpert.description_url_select)
                 .waitForExist(this.selector.BO.ModulePageSeoExpert.reference_url_select, 90000)
                 .click(this.selector.BO.ModulePageSeoExpert.reference_url_select)
-                .pause(2000)
-                .waitForExist(this.selector.BO.ModulePageSeoExpert.save_button, 90000)
-                .click(this.selector.BO.ModulePageSeoExpert.save_button)
-                .call(done)
-        })
-        it('should apply the rule', function (done) {
+                .click(done);
+        });
+
+        it('should save the url form', function (done) {
             global.fctname = this.test.title
             this.client
-                .pause(4000)
+                .waitForExist(this.selector.BO.ModulePageSeoExpert.save_button, 90000)
+                .click(this.selector.BO.ModulePageSeoExpert.save_button)
+                .call(done);
+        });
+
+        it('should close the validation message ', function (done) {
+            global.fctname = this.test.title;
+            this.client
+                .pause(2000)
                 .waitForExist(this.selector.BO.ModulePageSeoExpert.close_button, 9000)
                 .click(this.selector.BO.ModulePageSeoExpert.close_button)
-                .pause(5000)
-                // .waitForExist(this.selector.BO.ModulePageSeoExpert.apply_rule_button,9000)
+        });
+
+
+        it('should click on apply rule', function (done) {
+            global.fctname = this.test.title
+            this.client
                 .waitForExist('//*[@id="cat_5"]/td[7]/div/div/a', 5000)
                 //   .click(this.selector.BO.ModulePageSeoExpert.apply_rule_button)
                 .click('//*[@id="cat_5"]/td[7]/div/div/a')
                 .pause(7000)
                 .waitForExist(this.selector.BO.ModulePageSeoExpert.close_button, 9000)
                 .click(this.selector.BO.ModulePageSeoExpert.close_button)
-            .call(done)
-    })
-});
+                .call(done);
+        });
+    });
 
-
-describe('Log out in Back Office', function (done) {
-    it('should log out successfully in BO', function (done) {
-        global.fctname = this.test.title;
-        this.client
-            .signoutBO()
-            .call(done);
-    })
-});
+    describe('Log out in Back Office', function (done) {
+        it('should log out successfully in BO', function (done) {
+            global.fctname = this.test.title;
+            this.client
+                .signoutBO()
+                .call(done);
+        })
+    });
 
 
 })
--- seoExpert_module/test/scenario/FO/check_seo_expert_urls_webdriverio.js
+++ seoExpert_module/test/scenario/FO/check_seo_expert_urls_webdriverio.js
@@ -3,7 +3,7 @@ var should = require('should');
 var common = require('../../../../common/common.webdriverio');
 var globals = require('../../../../common/globals.webdriverio.js');
 
-describe('Test n°3 = Check the optimizing urls configuration', function () {
+describe('Test n°3 = Check the Url in Fo', function () {
     common.initMocha.call(this);
 
     before(function (done) {
@@ -13,43 +13,61 @@ describe('Test n°3 = Check the optimizing urls configuration', function () {
 
     after(common.after);
 
+    describe("Check the Url in FO", function (done) {
 
-describe("Check the Url in FO", function (done) {
 
+        it('should go to FO', function (done) {
+            global.fctname = this.test.title
+            this.client
+                .url('http://' + URL)
+                .waitForExist('//*[@id="content"]/section/div/article/div/a/img', 90000)
+                .click('//*[@id="content"]/section/div/article/div/a/img')
+                .call(done);
+        });
+        it('should get name product', function (done) {
+            global.fctname = this.test.title
+            this.client
+                .waitForExist('//*[@id="wrapper"]/div/nav/ol/li[2]/a/span', 9000)
+                .getText('//*[@id="wrapper"]/div/nav/ol/li[2]/a/span').then(function (text) {
+                global.title = text.toLowerCase();
+            })
+                .call(done);
+        });
+        it('should get description product', function (done) {
+            global.fctname = this.test.title
+            this.client
+                .waitForExist('//*[@id="main"]/div[1]/div[2]/div[2]/div[4]/ul/li[1]/a', 9000)
+                .click('//*[@id="main"]/div[1]/div[2]/div[2]/div[4]/ul/li[1]/a')
+                .getText('//*[@id="description"]/div/p').then(function (text) {
+                global.description = text.toLowerCase();
+            })
+                .call(done);
+        });
+        it('should get reference product', function (done) {
+            global.fctname = this.test.title
+            this.client
+                .waitForExist('//*[@id="main"]/div[1]/div[2]/div[2]/div[4]/ul/li[2]/a', 9000)
+                .click('//*[@id="main"]/div[1]/div[2]/div[2]/div[4]/ul/li[2]/a')
+                .getText('//*[@id="product-details"]/div[1]/span').then(function (text) {
+                console.log(text);
+                global.reference = text.toLowerCase();
+            })
+                .call(done);
 
-    it('should go to fo', function (done) {
-        global.fctname = this.test.title
-        this.client
-            .url('http://' + URL)
-            .waitForExist('//*[@id="content"]/section/div/article/div/a/img', 90000)
-            .click('//*[@id="content"]/section/div/article/div/a/img')
-            .pause(90000)
-            .waitForExist('//*[@id="wrapper"]/div/nav/ol/li[2]/a/span', 9000)
-            .getText('//*[@id="wrapper"]/div/nav/ol/li[2]/a/span').then(function (text) {
-            global.title = text.toLowerCase();
         })
-            .waitForExist('//*[@id="main"]/div[1]/div[2]/div[2]/div[4]/ul/li[1]/a', 9000)
-            .click('//*[@id="main"]/div[1]/div[2]/div[2]/div[4]/ul/li[1]/a')
-            .getText('//*[@id="description"]/div/p').then(function (text) {
-            global.description = text.toLowerCase();
-        })
-            .waitForExist('//*[@id="main"]/div[1]/div[2]/div[2]/div[4]/ul/li[2]/a', 9000)
-            .click('//*[@id="main"]/div[1]/div[2]/div[2]/div[4]/ul/li[2]/a')
-            .getText('//*[@id="product-details"]/div[1]/span').then(function (text) {
-            console.log(text);
-            global.reference = text.toLowerCase();
-        })
+        it('should check the url form ', function (done) {
+            global.fctname = this.test.title
+            this.client
+                .getUrl().then(function (url) {
+                var productUrl = url.split('1-').pop().split('.html').shift();
+                should(productUrl).be.equal(global.title + '-' + global.description + '-' + global.reference)
+            })
+                .pause(90000)
+                .call(done);
 
-            .getUrl().then(function (url) {
-            var productUrl = url.split('1-').pop().split('.html').shift();
-            should(productUrl).be.equal(global.title + '-' + global.description + '-' + global.reference)
-        })
-            .pause(90000)
-            .call(done)
+        });
 
-    })
-
-});
+    });
 
 
 })
